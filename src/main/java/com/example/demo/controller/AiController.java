bb80ec88e924d477242e33ea5ee642f15a1981c9
SJ <s4int.jay@gmail.com>
2026-03-14 09:51:25 +0900

    chapter01
    기본 페이지
    ollama 연결

---
+++ src/main/java/com/example/demo/controller/AiController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/ai")
+@Slf4j
+public class AiController {
+
+    @PostMapping(value = "/chat", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+    public String postMethodName(@RequestParam("question") String question) {
+        return "아직 모델과 연결되지 않았습니다.";
+    }
+
+}
