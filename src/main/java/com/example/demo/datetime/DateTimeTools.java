5b8ed1ecf44405825f0ea2703304965e4f7d1daf
SJ <s4int.jay@gmail.com>
2026-03-25 19:53:45 +0900

    chap11-3
    @Tool

---
+++ src/main/java/com/example/demo/datetime/DateTimeTools.java
@@ -0,0 +1,50 @@
+package com.example.demo.datetime;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+@Slf4j
+public class DateTimeTools {
+
+	@Tool(description = "현재 날짜와 시간 정보를 제공합니다.")
+	public String getCurrentDateTime() {
+		String nowTime = LocalDateTime.now()
+				.atZone(LocaleContextHolder.getTimeZone().toZoneId())
+				.toString();
+
+		log.info("현재 시간: {}", nowTime);
+
+		return nowTime;
+	}
+
+	@Tool(description = "지정된 시간에 알람을 설정합니다.")
+	public void setAlarm(
+			@ToolParam(description = "ISO-8601 형식의 시간", required = true)
+			String time
+	) {
+		if (time.contains("T24:")) {
+			int tIndex = time.indexOf("T24");
+			String datePart = time.substring(0, tIndex);
+			String timePart = time.substring(tIndex);
+
+			LocalDate date = LocalDate.parse(datePart);
+			date = date.plusDays(1);
+			
+			timePart = timePart.replaceFirst("T24:", "T00:");
+			time = date + timePart;
+		}
+
+		LocalDateTime alarmTime = LocalDateTime.parse(time,
+				DateTimeFormatter.ISO_DATE_TIME);
+
+		log.info("알람 설정 시간: {}", alarmTime);
+	}
+}
