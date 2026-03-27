9cdaa1f7a040ea680d8104a5c63023962aa30955
SJ <s4int.jay@gmail.com>
2026-03-27 14:06:01 +0900

    chap06-8
    ImagePrompt
    ImageResponse

--- src/main/java/com/example/demo/service/AiService.java
+++ src/main/java/com/example/demo/service/AiService.java
@@ -2,11 +2,17 @@ package com.example.demo.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.content.Media;
+import org.springframework.ai.image.ImageMessage;
 import org.springframework.ai.image.ImageModel;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MimeType;
@@ -22,7 +28,11 @@ public class AiService {
 	private ImageModel imageModel;
 
 	public AiService(ChatClient.Builder chatClientBuilder) {
-		this.chatClient = chatClientBuilder.build();
+		this.chatClient = chatClientBuilder
+				.defaultAdvisors(
+						new SimpleLoggerAdvisor(Ordered.LOWEST_PRECEDENCE - 1)
+				)
+				.build();
 	}
 
 	public Flux<String> imageAnalysis(String question, String contentType, byte[] bytes) {
@@ -49,5 +59,39 @@ public class AiService {
 		return flux;
 	}
 
-//	private String koToEn(String text) {}
+	private String koToEn(String text) {
+		String question = """
+				당신은 번역가입니다. 아래 한글 문장을 영어 문장으로 번역해주세요.
+				%s""".formatted(text);
+
+		String englishDescription = chatClient.prompt()
+				.user(question)
+				.call()
+				.content();
+
+		return englishDescription;
+	}
+
+	public String generateImage(String description) {
+		String englishDescription = koToEn(description);
+
+		ImageMessage imageMessage = new ImageMessage(englishDescription);
+
+		OpenAiImageOptions imageOptions = OpenAiImageOptions.builder()
+//				.model("gpt-image-1")
+//				.quality("low")
+//				.width(1536)
+				.model("dall-e-2")
+				.responseFormat("b64_json")
+				.width(1024)
+				.height(1024)
+				.N(1)
+				.build();
+
+		ImagePrompt imagePrompt = new ImagePrompt(imageMessage, imageOptions);
+		ImageResponse imageResponse = imageModel.call(imagePrompt);
+
+		String b64Json = imageResponse.getResult().getOutput().getB64Json();
+		return b64Json;
+	}
 }
